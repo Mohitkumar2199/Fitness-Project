b73da0737857a120f4f1cacba436df5a6e984ae8
Mohitkumar2199 <mohitk54079@gmail.com>
2026-06-18 16:01:23 +0530

    fix: add vercel domain to CORS

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://fitness-project-red.vercel.app", "http://localhost:3000"],
+  credentials: true,
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
